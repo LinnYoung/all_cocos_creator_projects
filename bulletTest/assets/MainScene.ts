c04fafb1376dcbc2d50c243f730c1f61908a1e6f
linn <946692837@qq.com>
2020-07-10 22:39:52 +0800

    炮台角度

--- bulletTest/assets/MainScene.ts
+++ bulletTest/assets/MainScene.ts
@@ -11,21 +11,20 @@ export default class MainScene extends cc.Component {
   @property(cc.Node)
   canno: cc.Node = null; // 炮塔
 
-  // 初始坐标值（因为炮台的锚点坐标）我这里屏幕中间最下方（0，width/2)
-  private _initialPos: cc.Vec2 = null; // cc.v2(0, -this.node.width / 2);
+  // 初始坐标值（因为炮台的锚点坐标）我这里屏幕中间最下方（0，height/2)
+  private _initialPos: cc.Vec2 = null;
 
   onLoad() {
     this._initialPos = cc.v2(0, -this.node.height / 2);
     this.node.on(cc.Node.EventType.TOUCH_START, this.emitBullte, this);
   }
 
+  // 炮塔及发射角度
   private emitBullte(event: cc.Touch) {
     const pos = this.content.convertToNodeSpaceAR(event.getLocation());
     const newPos = pos.sub(this._initialPos);
     let radian = newPos.signAngle(cc.v2(0, 1));
-    const newAngle = (radian * 180) / Math.PI + 180;
-
-    this.canno.angle = newAngle;
-    cc.log("angle; ", this.canno.angle);
+    const newAngle = (radian * 180) / Math.PI;
+    this.canno.angle = -newAngle;
   }
 }
